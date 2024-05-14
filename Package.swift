0a21c69955fdd8c3849113643a328815fcc7edde
Marcus Wu <marcus.wu0814@gmail.com>
2024-05-14 14:08:48 +0800

    Update SPM tool version

--- Package.swift
+++ Package.swift
@@ -1,5 +1,15 @@
+// swift-tools-version:5.10
 import PackageDescription
 
 let package = Package(
-    name: "RangeSeekSlider"
+    name: "RangeSeekSlider",
+    platforms: [
+        .iOS(.v14)
+    ],
+    products: [
+        .library(name: "RangeSeekSlider", targets: ["RangeSeekSlider"])
+    ],
+    targets: [
+        .target(name: "RangeSeekSlider", path: "Sources")
+    ]
 )
